82d586cd7304fa2d24bb7409e2c67618b84d8251
Victor Kinyanjui Ngigi <victor.ngigi@outlook.com>
2025-03-11 10:00:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 1000);
+console.log(random);
+
+let num3 = 34;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(10,15,17,20);
+console.log(max);
+
+
